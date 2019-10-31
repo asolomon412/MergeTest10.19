b7c90504e7d7dd5784747fa485fd4a6595fe4c9b
Antonella Solomon <antonellasolomon@gmail.com>
2019-10-31 09:14:56 -0400

    initial commit

---
+++ src/co/gc/MainApp.java
@@ -0,0 +1,11 @@
+package co.gc;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("My name is Antonella!");
+
+	}
+
+}
