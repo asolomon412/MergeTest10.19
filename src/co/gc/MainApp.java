345c7bef60a1a46b9c91832cf1ac44838709d58b
kyledexperience <kyledexperience@gmail.com>
2019-10-31 09:19:10 -0400

    added some things

--- src/co/gc/MainApp.java
+++ src/co/gc/MainApp.java
@@ -5,6 +5,8 @@ public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("My name is Antonella!");
+		
+		System.out.println("Hello, my name is Kyle. Happy Hallows");
 
 	}
 
